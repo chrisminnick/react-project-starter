ab5a9847dd33446f92c51da90e44483af4eb8a8b
Chris Minnick <chris@minnick.com>
2016-03-19 14:45:32 -0700

    added shell scripts

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ var gulp = require('gulp');
 
 gulp.task('default', function() {
 
-    
+//todo: run npm rebuild if .bin directory isn't present (new download from repo, for example)
+
+
+
 console.log('\n\nBUILD OK');
 });
