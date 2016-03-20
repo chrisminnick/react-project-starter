17b5278283827e50fa378a662f3c42ee46ef11bf
Chris Minnick <chris@minnick.com>
2016-03-20 14:10:29 -0700

    added a test

--- src/test.js
+++ src/test.js
@@ -4,16 +4,11 @@
 (() => {
 "use strict";
 
-    var jasmine = require("jasmine");
+    var assert = require("chai").assert;
 
+    // basic addition
     assert.equal(add(3,4), 7);
-
-    var result = add(3,4);
-    var expected = 7;
-
-    if (result !== expected){
-        throw new Error("Expected " + expected + ", but got " + result);
-    }
+    
 
     function add(a,b) {
         return a + b;
