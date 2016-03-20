fb261a282f23a9250c6a957b9896f10d55dc5be7
Chris Minnick <chris@minnick.com>
2016-03-20 13:50:16 -0700

    installed gulp-jasmine

--- src/test.js
+++ src/test.js
@@ -4,6 +4,15 @@
 (() => {
 "use strict";
 
+    var result = add(3,4);
+    var expected = 7;
 
+    if (result !== expected){
+        throw new Error("Expected " + expected + ", but got " + result);
+    }
+
+    function add(a,b) {
+        return a - b;
+    }
 
 })();
