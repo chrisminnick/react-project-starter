9b15d9c7a28e4df20008126e01da44b51882dbe8
chrisminnick <chris@watzthis.com>
2016-03-30 13:10:19 -0700

    renamed sayHello function to greet

--- spec/sayHelloSpec.js
+++ spec/sayHelloSpec.js
@@ -1,22 +1,16 @@
 (function() {
     "use strict";
 
-    function sayHello(name) {
-        if (name) {
-            return "Hello, " + name;
-        } else {
-            return "Hello";
-        }
-    }
+    var sayHello = require("../src/sayHello.js");
 
-    describe("Say Hello", function() {
+    describe("Greet", function() {
         it("concats Hello and a name", function() {
-            let actual = sayHello("World");
+            let actual = sayHello.greet("World");
             let expected = "Hello, World";
             expect(actual).toEqual(expected);
         });
         it("handles undefined argument", function() {
-            let actual = sayHello();
+            let actual = sayHello.greet();
             let expected = "Hello";
             expect(actual).toEqual(expected);
         });
---
+++ src/sayHello.js
@@ -0,0 +1,13 @@
+/**
+ * Created by chrisjminnick on 3/30/16.
+ */
+(function() {
+    "use strict";
+    function greet(name) {
+        if (name) {
+            return "Hello, " + name;
+        } else {
+            return "Hello";
+        }
+    }
+});
